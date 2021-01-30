bb43452037778bddc241bd9eb6087ff0a351fb3c
shaikh <shaikhsa7lim@gmail.com>
2021-01-30 14:48:16 +0530

    discharge

--- dischargeipdoneday2.php
+++ dischargeipdoneday2.php
@@ -1,4 +1,68 @@
 <?php
+function checkday($admited,$current)
+{
+   
+   
+   
+    $date11=date_create($admited);
+    $date22=date_create($current);
+    $diff=date_diff($date11,$date22);
+    echo " ".$current;
+    echo " ".$admited;
+    $di=$diff->format("%R%a days");
+    echo $di;
+    echo "come";
+   
+    return $di;
+}
+function randomdicharge($n)
+{   global $mysqli,$date2,$date,$status;
+   
+
+    $sql="select Ipdnumber,admittedate2,admindate from ipdgenralinfoold where `pstatus2` = 'Admitted' AND `pdate2` = '$date2' order by rand()";
+    $result=$mysqli->query($sql);
+    
+    while(($row=$result->fetch_Array(MYSQLI_NUM)) && $n>=1)
+    {  
+
+        $diff=checkday($row[1],$date);
+    
+        
+                if($diff>=6)
+                    { echo "Random discharge".$n;
+                        $sql="UPDATE `ipdgenralinfoold` SET `pstatus2`='discharge',`disdate`='$date',`disdate2`='$date2' WHERE `Ipdnumber`='$row[0]' and pdate='$date'";
+                        if($result2=$mysqli->query($sql))
+                        {
+                                echo "update in ipd";
+                        }
+                        else{
+                            echo"<h1>wrong</h1>";
+                        }
+                        
+                        $sql="update tvd_bed_avialbel_chk set status='0' where Ipdnumber='$row[0]'";
+                        if($res=$mysqli->query($sql))
+                        {
+                            echo "update in table bed";
+                        }
+                        else{
+                            echo"wrong in table bed";
+                        }
+                        $n-=1;
+                    }
+            
+        
+        }         
+   
+}
+
+
+
+
+
+
+
+
+
     
     function discharge($n,$method)
     { 
@@ -11,6 +75,10 @@
       if($method)
       {
         $sql="SELECT  `Ipdnumber`  FROM `ipdgenralinfoold` WHERE `pstatus2` = 'Admitted' AND `pdate2` = '$date2' LIMIT $n";
+      }
+      else{
+        randomdicharge($n);
+        return 0;
       }
      
        
@@ -44,6 +112,7 @@
         else{
             echo "not going";
         }
+        return 0;
     }
 
 
@@ -59,8 +128,8 @@
     }
     else{
    echo"connection established";
-   discharge($n,1);
-   ;
+      $res=discharge($n,1);
+        $res=discharge($n,0);
 
 
 
